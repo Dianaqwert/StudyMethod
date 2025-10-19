4c897936f227bfe9bf91432082fba33d0f4dbfa3
Asus Ryzen 5 <esmeralda298rivera@gmail.com>
2025-10-18 21:11:11 -0600

    Screen that displays flashcard statistics

--- app/src/main/java/com/example/proyecto_final/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_final/MainActivity.kt
@@ -10,8 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.pantallajuegoflashcards)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.juegoFC)) { v, insets ->
+        setContentView(R.layout.pantallaestadiscticasflashcards)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.pantallaEstd)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
