675148f1845cf4763320a2cff6458fd4071d4033
Asus Ryzen 5 <esmeralda298rivera@gmail.com>
2025-11-02 23:24:19 -0600

    App navigation flow

---
+++ app/src/main/java/com/example/proyecto_final/Act8Estadisticas.kt
@@ -0,0 +1,13 @@
+package com.example.proyecto_final
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Act8Estadisticas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
